d9ae87faa239e13407c49909df67b59754f42d23
srenault-efrei <56259187+srenault-efrei@users.noreply.github.com>
2021-03-07 16:35:08 +0100

    Delete .build directory

--- .build/definitions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var State;
-
-(function (State) {
-  State[State["Open"] = 0] = "Open";
-  State[State["Wating"] = 1] = "Wating";
-  State[State["Close"] = 2] = "Close";
-})(State || (State = {}));
